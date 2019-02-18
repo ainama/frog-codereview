df31aae03642a1922f2526ba7b0476ace59a424a
刘翠玲 <liucuiling1@liucuilingMacBook-Pro.local>
2019-02-18 15:32:14 +0800

    input组件样式改进，图标样式以及功能，button点击功能

--- demo/src/demo10/myInput.js
+++ demo/src/demo10/myInput.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import personicon from '../img/person.svg';
+import searchicon from '../img/search.png';
+
 import './sass/input.scss';
+
 
 export default class Input extends Component {
   constructor(porps, context) {
     super(porps);
+    this.state = {
+      focus: false
+    };
+  }
+
+  _inputonfocus() {
+    this.setState({
+      focus: true,
+    });
+  }
+
+  _inputFocusBorder() {
+    this.refs.myInput.focus();
+    this.setState({
+      focus: true,
+    });
+    //console.log(this.props.value);
+  }
+
+  focus() {
+    this.setState({
+      focus: true,
+    });
+  }
+
+  _without() {
+
   }
 
   render() {
     let {
+      value,
       placeholder,
       title,
       round,
@@ -22,100 +54,220 @@ export default class Input extends Component {
       onChange,
       disabled,
       pictureicon,
-      rightButton
+      rightButton,
+      //inputStyle,
+      onSearch,
+      replaceNormaltext,
+      leftIconUrl,
+      iconLeftStyle,
+      inputWidth,
+      inputHeight,
+      rightIconUrl,
+      iconRightStyle,
+      onFocus,
+      defaultValue,
+      onInput
+
     } = this.props;
 
+    let { focus } = this.state;
+
+    let iconBackground = {};
+
+    if (inputWidth !== undefined || inputHeight !== undefined) {
+      iconBackground = {
+        width: inputWidth,
+        height: inputHeight
+      };
+    } else {
+      iconBackground = {
+        width: '220px',
+        height: '30px'
+      };
+    }
+
+    if (pictureicon === 'iconleft') {
+      if (leftIconUrl === undefined) {
+        iconBackground = {
+          width: inputWidth === undefined ? '220px' : inputWidth,
+          height: inputHeight === undefined ? '30px' : inputHeight,
+          backgroundImage: `url(${ personicon })`,
+          backgroundSize: iconLeftStyle === undefined ? '15px 12px' : iconLeftStyle,
+          backgroundPosition: '11px center',
+          backgroundRepeat: 'no-repeat',
+        }
+      } else {
+        iconBackground = {
+          width: inputWidth,
+          height: inputHeight,
+          backgroundImage: `url(${ leftIconUrl })`,
+          backgroundSize: iconLeftStyle === undefined ? '15px 12px' : iconLeftStyle,
+          backgroundPosition: '11px center',
+          backgroundRepeat: 'no-repeat',
+        };
+      }
+    } else if (pictureicon === 'iconright') {
+
+      if (rightIconUrl === undefined) {
+        iconBackground = {
+          width: inputWidth,
+          height: inputHeight,
+          backgroundImage: `url(${ searchicon })`,
+          backgroundSize: iconRightStyle === undefined ? '14px 14px' : iconRightStyle,
+          backgroundPosition: '95% center',
+          backgroundRepeat: 'no-repeat',
+        }
+
+      } else {
+        iconBackground = {
+          width: inputWidth,
+          height: inputHeight,
+          backgroundImage: `url(${ rightIconUrl })`,
+          backgroundSize: iconRightStyle === undefined ? '14px 14px' : iconRightStyle,
+          backgroundPosition: '95% center',
+          backgroundRepeat: 'no-repeat',
+        }
+      }
+    }
 
     return (
       <div className = { `box ${ layout ? 'box-column' : 'box-row' }` }>
-        {
-          round && (
-            <div className = 'round-small'></div>
-          )
-        }
-        {
-          title !== undefined && (
-            <div
-              className = { `
-              textname
-              ${ layout ? 'textname-top' : 'textname-left' }
-              ` }>
-              { title }
-            </div>
-          )
-        }
+        <div className = { ` box-row ${ layout ? 'box-round' : 'box-normal' }` }>
+          {
+            round && (
+              <div className = 'round-small'></div>
+            )
+          }
+          {
+            title !== undefined && (
+              <div
+                className =
+                  { `textname ${ round ? 'textname-left' : 'textname-right' }` }>
+                { title }
+              </div>
+            )
+          }
+        </div>
         <div
-          className = { `
-        ${ wrongtext ? 'wronginputstyle' : 'inputstyle' }
-        ${
-            pictureicon === undefined ? '' :
-            pictureicon === 'iconleft' ? 'personurl personurl-left' :
-            pictureicon === 'iconright' ? 'personurl personurl-right' : ''
-          }` }>
-          <input
+          className = { ` box-normal ${ normaltextBottom ? 'box-column' : ' box-row' } ${ rightButton ? ' box-row ' : '' }` }>
+
+          <div
             className = { `
+              ${ wrongtext ? 'wronginputstyle' : 'inputstyle' }
+              ${ pictureicon === 'iconleft' ? ' personurl' : '' }
+              ${ focus ? 'inputonfocus' : '' }
+              ` }>
+
+            <input
+              style = { iconBackground }
+              className = { `
               ${ disabled ? 'input-disabled' : '' }
               ${ rightButton === undefined ? '' : rightButton.length > 0 ? 'input-border-right' : '' }
               ` }
-            disabled = { disabled }
-            type = { type }
-            placeholder = { placeholder }
-            maxLength = { maxLength }
-            onChange = { onChange }
-          />
+              disabled = { disabled }
+              type = { type }
+              placeholder = { placeholder }
+              maxLength = { maxLength }
+              onChange = { onChange }
+              value = { value }
+              onClick = { onFocus ? () => {
+                this._inputonfocus()
+              } : this._without() }
+              ref = 'myInput'
+              defaultValue = { defaultValue }
+              onInput = { onInput }
+
+            />
+            {
+              layout === undefined && normaltextBottom &&
+              (
+                <div
+                  className = { `
+                    pointout
+                    pointout-position-bottom
+                    ${ wrongtext ? 'wrong' : 'normal' }` }>
+                  { wrongtext ? replaceNormaltext ? replaceNormaltext : normaltext : normaltext }
+                </div>
+              )
+            }
+          </div>
           {
-            layout &&
+            layout && rightButton === undefined &&
             (
               <div
                 className = { `
                   pointout
-                  ${ normaltextBottom ? 'pointout-position-bottom' : 'pointout-position-right' }
+                  ${ normaltextBottom ? 'pointout-bottom' : 'pointout-right' }
                   ${ wrongtext ? 'wrong' : 'normal' }
                   ` }>
-                  { normaltext }
+                { wrongtext ? replaceNormaltext ? replaceNormaltext : normaltext : normaltext }
               </div>
             )
           }
           {
-            layout === undefined && normaltextBottom &&
+            layout === undefined && normaltextBottom === undefined && rightButton === undefined &&
             (
               <div
                 className = { `
                   pointout
-                  pointout-position-bottom
+                  pointout-right
                   ${ wrongtext ? 'wrong' : 'normal' }
                   ` }>
-                  { normaltext }
+                { wrongtext ? replaceNormaltext ? replaceNormaltext : normaltext : normaltext }
+              </div>
+            )
+          }
+          {
+            rightButton !== undefined && rightButton.length > 0 &&
+            (
+              <button
+                className = 'button-submit'
+                onClick = { onSearch ? () => this._inputFocusBorder() : this._without() }>{ rightButton }</button>
+            )
+          }
+          {
+            layout && rightButton !== undefined && normaltextBottom === undefined &&
+            (
+              <div
+                className = { `
+                  pointout
+                  ${ normaltextBottom ? 'pointout-bottom' : 'pointout-right' }
+                  ${ wrongtext ? 'wrong' : 'normal' }
+                  ` }>
+                { wrongtext ? replaceNormaltext ? replaceNormaltext : normaltext : normaltext }
               </div>
             )
           }
         </div>
         {
-          layout === undefined && normaltextBottom === undefined && rightButton === undefined &&
+          layout && rightButton !== undefined && normaltextBottom &&
           (
             <div
               className = { `
-                pointout
-                pointout-right
-                ${ wrongtext ? 'wrong' : 'normal' }
-                ` }>
-                { normaltext }
+                  pointout
+                   ${ normaltextBottom ? 'pointout-bottom' : '' }
+                  ${ wrongtext ? 'wrong' : 'normal' }
+                  ` }>
+              { wrongtext ? replaceNormaltext ? replaceNormaltext : normaltext : normaltext }
             </div>
           )
         }
-        {
-          rightButton !== undefined && rightButton.length > 0 &&
-          (
-            <button className = 'button-submit'>{ rightButton }</button>
-          )
-        }
       </div>
-
     )
   }
 }
+Input.defaultProps = {
+  round: false,
+  onFocus: false,
+  wrongtext: false
+};
 Input.propTypes = {
-  //layout: PropTypes.bool,
+  round: PropTypes.bool,
+  onFocus: PropTypes.bool,
+  wrongtext: PropTypes.bool,
   //maxLength: PropTypes.number,
-  rightButton: PropTypes.string
+  rightButton: PropTypes.string,
+  pictureicon: PropTypes.string,
+  //defaultValue: PropTypes.string,
+  iconBackground: PropTypes.object,
 };
--- demo/src/demo10/page.js
+++ demo/src/demo10/page.js
@@ -5,41 +5,207 @@ import Input from "./myInput.js";
 export default class Page extends Component {
   constructor(porps, context) {
     super(porps);
-    this.state = {};
+    this.state = {
+      value: ''
+    };
   }
 
   _handleInputname = (e) => {
     let _value = e.target.value;
     let pattern = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？%]");
     let result = _value.match(pattern);
+    if (result === null || result === 'null') {
+      this.setState({
+        wrongtext: false,
+      })
+    } else {
+      this.setState({
+        wrongtext: true,
+      })
+    }
     this.setState({
-      wrongtext: result
+      value: _value
     })
   };
 
+  _myFunction = (e) => {
+    //console.log(e.target.value);
+  };
+
   render() {
+    let { value } = this.state;
+
     return (
       <div>
         <Input
           type = 'text'
           placeholder = 'Username' // input框默认内容
-          maxLength = '10'  //字体限制字数
-          //round = { this.props.round }   //圆点
+          defaultValue = 'hello'
+          //onChange = { this._handleInputname }
+          //value = { value }   //input 的value值
+          onFocus = { true }
+          //onInput = { this._myFunction }
+          //inputWidth = '400px'
+          //inputHeight = '50px'
+          //maxLength = '10'  //字体限制字数
+          round = { true }   //圆点
           title = '姓名'    //标题:默认左侧
           //layout   //布局标题位于上面
-          normaltext = '这是提示文字' //正常提示文字:默认位于右侧
+          normaltext = '请不要输入特殊字符' //正常提示文字:默认位于右侧
+          //replaceNormaltext = '已输入特殊字符'
+          //wrongtext = { this.state.wrongtext } //错误提示文字
           //normaltextBottom  //提示文字：位于下面
-          onChange = { this._handleInputname }
-          wrongtext = { this.state.wrongtext } //错误提示文字
           //disabled   //禁止状态
-          //pictureicon='iconleft'  //input框左侧按钮
-          //pictureicon = 'iconright'  //inout右侧按钮
-          //rightButton = 'Sumbit'  //右侧button按钮
+          //pictureicon = 'iconleft'  //input框左侧按钮
+          //leftIconUrl = { require('../img/finish.png') }
+          //iconLeftStyle = '15px 15px'
+          //pictureicon = 'iconright'  //input框左侧按钮
+          //rightIconUrl = { require('../img/finish.png') }
+          //iconRightStyle = '15px 15px'
+          //rightButton = 'Submit'  //右侧button按钮
+          //onSearch = { true }
+        />
+        <Input
+          type = 'text'
+          placeholder = 'Username' // input框默认内容
+          //defaultValue = 'hello'
+          //onChange = { this._handleInputname }
+          //value = { value }   //input 的value值
+          onFocus = { true }
+          //onInput = { this._myFunction }
+          //inputWidth = '400px'
+          //inputHeight = '50px'
+          maxLength = '10'  //字体限制字数
+          round = { false }   //圆点
+          title = '姓名'    //标题:默认左侧
+          //layout   //布局标题位于上面
+          normaltext = '请不要输入特殊字符' //正常提示文字:默认位于右侧
+          replaceNormaltext = '已输入特殊字符'
+          //wrongtext = { this.state.wrongtext } //错误提示文字
+          normaltextBottom  //提示文字：位于下面
+          //disabled   //禁止状态
+          //pictureicon = 'iconleft'  //input框左侧按钮
+          //leftIconUrl = { require('../img/finish.png') }
+          //iconLeftStyle = '15px 15px'
+          //pictureicon = 'iconright'  //input框左侧按钮
+          //rightIconUrl = { require('../img/finish.png') }
+          //iconRightStyle = '15px 15px'
+          rightButton = 'Submit'  //右侧button按钮
+          onSearch = { true }
+        />
+        <Input
+          type = 'text'
+          placeholder = 'Username' // input框默认内容
+          //defaultValue = 'hello'
+          onChange = { this._handleInputname }
+          value = { value }   //input 的value值
+          onFocus = { true }
+          //onInput = { this._myFunction }
+          //inputWidth = '400px'
+          //inputHeight = '50px'
+          maxLength = '10'  //字体限制字数
+          round = { true }   //圆点
+          title = '姓名'    //标题:默认左侧
+          layout   //布局标题位于上面
+          normaltext = '请不要输入特殊字符' //正常提示文字:默认位于右侧
+          replaceNormaltext = '已输入特殊字符'
+          wrongtext = { this.state.wrongtext } //错误提示文字
+          //normaltextBottom  //提示文字：位于下面
+          //disabled   //禁止状态
+          //pictureicon = 'iconleft'  //input框左侧按钮
+          //leftIconUrl = { require('../img/finish.png') }
+          //iconLeftStyle = '15px 15px'
+          //pictureicon = 'iconright'  //input框左侧按钮
+          //rightIconUrl = { require('../img/finish.png') }
+          //iconRightStyle = '15px 15px'
+          //rightButton = 'Submit'  //右侧button按钮
+          //onSearch = { true }
+        />
+        <Input
+          type = 'text'
+          placeholder = 'Username' // input框默认内容
+          //defaultValue = 'hello'
+          //onChange = { this._handleInputname }
+          //value = { value }   //input 的value值
+          onFocus = { true }
+          //onInput = { this._myFunction }
+          //inputWidth = '400px'
+          //inputHeight = '50px'
+          maxLength = '10'  //字体限制字数
+          round = { false }   //圆点
+          title = '姓名'    //标题:默认左侧
+          layout   //布局标题位于上面
+          normaltext = '请不要输入特殊字符' //正常提示文字:默认位于右侧
+          //replaceNormaltext = '已输入特殊字符'
+          //wrongtext = { this.state.wrongtext } //错误提示文字
+          normaltextBottom  //提示文字：位于下面
+          //disabled   //禁止状态
+          //pictureicon = 'iconleft'  //input框左侧按钮
+          //leftIconUrl = { require('../img/finish.png') }
+          //iconLeftStyle = '15px 15px'
+          //pictureicon = 'iconright'  //input框左侧按钮
+          //rightIconUrl = { require('../img/finish.png') }
+          //iconRightStyle = '15px 15px'
+          rightButton = 'Submit'  //右侧button按钮
+          onSearch = { true }
+        />
+        <Input
+          type = 'text'
+          placeholder = 'Username' // input框默认内容
+          //defaultValue = 'hello'
+          //onChange = { this._handleInputname }
+          //value = { value }   //input 的value值
+          onFocus = { true }
+          //onInput = { this._myFunction }
+          //inputWidth = '400px'
+          //inputHeight = '50px'
+          maxLength = '10'  //字体限制字数
+          round = { true }   //圆点
+          title = '姓名'    //标题:默认左侧
+          //layout   //布局标题位于上面
+          //normaltext = '请不要输入特殊字符' //正常提示文字:默认位于右侧
+          //replaceNormaltext = '已输入特殊字符'
+          //wrongtext = { this.state.wrongtext } //错误提示文字
+          //normaltextBottom  //提示文字：位于下面
+          //disabled   //禁止状态
+          pictureicon = 'iconleft'  //input框左侧按钮
+          //leftIconUrl = { require('../img/finish.png') }
+          //iconLeftStyle = '15px 15px'
+          //pictureicon = 'iconright'  //input框左侧按钮
+          //rightIconUrl = { require('../img/finish.png') }
+          //iconRightStyle = '15px 15px'
+          rightButton = 'Submit'  //右侧button按钮
+          onSearch = { true }
+        />
+        <Input
+          type = 'text'
+          placeholder = 'Username' // input框默认内容
+          //defaultValue = 'hello'
+          //onChange = { this._handleInputname }
+          //value = { value }   //input 的value值
+          onFocus = { true }
+          //onInput = { this._myFunction }
+          //inputWidth = '400px'
+          //inputHeight = '50px'
+          maxLength = '10'  //字体限制字数
+          round = { true }   //圆点
+          title = '姓名'    //标题:默认左侧
+          layout   //布局标题位于上面
+          //normaltext = '请不要输入特殊字符' //正常提示文字:默认位于右侧
+          //replaceNormaltext = '已输入特殊字符'
+          //wrongtext = { this.state.wrongtext } //错误提示文字
+          //normaltextBottom  //提示文字：位于下面
+          //disabled   //禁止状态
+          //pictureicon = 'iconleft'  //input框左侧按钮
+          //leftIconUrl = { require('../img/finish.png') }
+          //iconLeftStyle = '15px 15px'
+          pictureicon = 'iconright'  //input框左侧按钮
+          //rightIconUrl = { require('../img/finish.png') }
+          //iconRightStyle = '15px 15px'
+          rightButton = 'Submit'  //右侧button按钮
+          onSearch = { true }
         />
       </div>
     )
   }
 }
-Page.defaultProps = {
-  round: false
-};
